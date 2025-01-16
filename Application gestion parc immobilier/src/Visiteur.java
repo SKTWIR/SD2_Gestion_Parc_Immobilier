b1bcc05bcaf2673bee09a7356d87ecc48e8cdd26
Ibrahim <159002102+SKTWIR@users.noreply.github.com>
2025-01-16 16:57:21 +0100

    Update Visiteur.java

--- Application gestion parc immobilier/src/Visiteur.java
+++ Application gestion parc immobilier/src/Visiteur.java
@@ -1,4 +1,7 @@
 //rôle de base de l'application, ce dernier peut réaliser des demandes de location et d'achat
 public class Visiteur extends Utilisateur {
-	
+	String idVisiteur;
+	String nom;
+	String prenom;
+	String email;
 }
