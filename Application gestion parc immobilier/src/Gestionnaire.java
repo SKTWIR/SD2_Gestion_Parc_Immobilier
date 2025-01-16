204f7b7cb770f8c77d448bc5692ba858c852c05f
Ibrahim <159002102+SKTWIR@users.noreply.github.com>
2025-01-16 17:12:14 +0100

    création de la classe gestionnaire
    la classe gestionnaire est la class TROP NUL qui s'occupe de la gestion des biens.

---
+++ Application gestion parc immobilier/src/Gestionnaire.java
@@ -0,0 +1,11 @@
+//création de la classe gestionnaire qui s'occupe de gérer les différents biens
+
+public class Gestionnaire {
+	
+	String idGestionnaire;
+	String nom;
+	String prenom;
+	String email;
+	
+	
+}
